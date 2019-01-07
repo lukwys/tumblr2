cdd5bb03496dbcc3ef479137fea2a04f4b567c30
Lukasz Wysocki <lwyso@softserveinc.com>
2019-01-07 16:54:50 +0100

    cr fix

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,21 +8,19 @@ import { Tags } from './Tags';
 
 export class Post extends PureComponent {
 
-    results = this.getPost();
-
-    getPost () {
+    get post () {
         return response.find((el) => 
-            el.posts.id == this.props.match.params.id
+            el.posts.id === +this.props.match.params.id
         )
     }
 
     render() {
         return (
             <div>
-                <PostType post={this.results} />
-                <Notes number={this.results.posts.note_count} />
-                <DateAndShare date={this.results.posts.date} />
-                <Tags tags={this.results.posts.tags} />
+                <PostType post={this.post} />
+                <Notes number={this.post.posts.note_count} />
+                <DateAndShare date={this.post.posts.date} />
+                <Tags tags={this.post.posts.tags} />
             </div>
         );
     }
