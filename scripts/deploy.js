0e95a2412ee6240ccc9de5501d5e305b28bb660e
Lukasz Wysocki <lwyso@softserveinc.com>
2019-01-14 23:01:20 +0100

    remove scripts

--- scripts/deploy.js
+++
@@ -1,9 +0,0 @@
-const ghpages = require('gh-pages');
-
-ghpages.publish('build', {
-    branch: 'master',
-    repo: 'https://github.com/lukwys/tumblr2.git'
-  }, (...args) => {
-    console.log(args);
-  });
-  
