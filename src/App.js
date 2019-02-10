20f5e1788ca5a45a1942f5a9475adbccce51af8a
lukwys <lukasz-wysocki18@wp.pl>
2019-02-10 20:12:12 +0100

    Revert "feature/add-craco (resolve#21)"

--- src/App.js
+++ src/App.js
@@ -4,36 +4,28 @@ import { Nav, NavItem } from 'reactstrap';
 import { Home } from './components/Home';
 import { PostList } from './components/PostList';
 import { Post } from './components/Post';
-import { Provider } from 'mobx-react';
-import { Store } from './Store';
 
-import 'bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.scss';
 
 class App extends Component {
-  componentWillMount() {
-    this.postStore = new Store();
-  }
-
   render() {
     return (
-      <Provider postStore = {this.postStore}>
-        <BrowserRouter>
-          <div className='app'>
-            <Nav className='app__nav'>
-              <NavItem>
-                <Link to="/" className='app__nav-link'>Home</Link>
-              </NavItem>
-              <NavItem>
-                <Link to='/Posts' className='app__nav-link'>Post list</Link>
-              </NavItem>
-            </Nav>
-            <Route exact path='/' component={Home} />
-            <Route path='/posts' component={PostList} />
-            <Route path='/post/:id' component={Post} />
-          </div>
-        </BrowserRouter>
-      </Provider>
+      <BrowserRouter>
+        <div className='app'>
+          <Nav className='app__nav'>
+            <NavItem>
+              <Link to="/" className='app__nav-link'>Home</Link>
+            </NavItem>
+            <NavItem>
+              <Link to='/Posts' className='app__nav-link'>Post list</Link>
+            </NavItem>
+          </Nav>
+          <Route exact path='/' component={Home} />
+          <Route path='/posts' component={PostList} />
+          <Route path='/post/:id' component={Post} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
